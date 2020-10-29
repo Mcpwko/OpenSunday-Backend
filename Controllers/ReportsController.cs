9c1ee8d351de9081ed236915378efca27b598218
Mcpwko <Mcpwko@users.noreply.github.com>
2020-10-29 09:36:00 +0100

    Report Table Added

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using opensunday_backend.Models;
+using OpenSundayApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenSundayApi.Controllers
+{
+    #region ReportsController
+    [Route("api/[controller]")]
+  [ApiController]
+  public class ReportsController : ControllerBase
+  {
+    private readonly OpenSundayContext _context;
+
+    public ReportsController(OpenSundayContext context)
+    {
+      _context = context;
+    }
+    #endregion
+
+    // GET: api/Reports
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Report>>> GetReports()
+    {
+            return await _context.Reports.Include(report => report.PlaceSet)
+                .Include(report => report.UserSet).ToListAsync();
+    }
+
+    #region snippet_GetByID
+    // GET: api/Reports/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Report>> GetReport(long id)
+    {
+      var report = await _context.Reports.FindAsync(id);
+
+      if (report == null)
+      {
+        return NotFound();
+      }
+
+      return report;
+    }
+    #endregion
+
+    #region snippet_Create
+    // POST: api/Reports
+    [HttpPost]
+    public async Task<ActionResult<Report>> PostReport(Report report)
+    {
+      report.ReportDate = DateTime.Now;
+
+      _context.Reports.Add(report);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetReport), new { id = report.IdReport }, report);
+    }
+    #endregion
+
+    #region snippet_Delete
+    // DELETE: api/Reports/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Report>> DeleteReport(long id)
+    {
+      var report = await _context.Reports.FindAsync(id);
+      if (report == null)
+      {
+        return NotFound();
+      }
+
+      _context.Reports.Remove(report);
+      await _context.SaveChangesAsync();
+
+      return report;
+    }
+    #endregion
+
+
+    private bool ReportExists(long id)
+    {
+      return _context.Reports.Any(e => e.IdReport == id);
+    }
+
+    }
+}
---
+++ Models/Report.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace opensunday_backend.Models
+{
+    public class Report
+    {
+        [Key]
+        public long IdReport { get; set; }
+        public string Comment { get; set; }
+        public bool IsForDelete { get; set; }
+        public bool IsForEdit { get; set; }
+        public DateTime ReportDate { get; set; }
+
+        [ForeignKey("UserSet")]
+        public long IdUser { get; set; }
+        public virtual User UserSet { get; set; }
+
+        [ForeignKey("PlaceSet")]
+        public long IdPlace { get; set; }
+        public virtual Place PlaceSet { get; set; }
+
+    }
+}
