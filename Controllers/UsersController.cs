ce0d957a795641cd4783f9042e46674846e00691
Mcpwko <Mcpwko@users.noreply.github.com>
2020-11-03 17:01:56 +0100

    add reportset to user

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -55,7 +55,7 @@ namespace OpenSundayApi.Controllers
     [HttpGet("{email}")]
     public async Task<ActionResult<User>> GetUser(string email)
     {
-      var users = await _context.Users.Include(user => user.ReviewSet).ToListAsync();
+      var users = await _context.Users.Include(user => user.ReviewSet).Include(user => user.ReportSet).ToListAsync();
       var user = users.Where(x => x.Email.Equals(email));
 
       if (user == null)
